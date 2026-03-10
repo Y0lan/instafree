2d5e0cbd1f0525caec90c17962bfce4f1d5584bf
Yolan Maldonado <yolanmaldonado96@gmail.com>
2026-03-10 14:11:31 +1100

    feat: add manifest patcher for InstaFreeSettings activity

---
+++ patch_manifest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Register InstaFreeSettings activity in AndroidManifest.xml."""
+import sys
+
+def patch_manifest(manifest_path):
+    with open(manifest_path, 'r') as f:
+        content = f.read()
+
+    if 'InstaFreeSettings' in content:
+        print("  Already patched: AndroidManifest.xml")
+        return True
+
+    # Insert before closing </application> tag
+    activity_tag = '''
+        <activity
+            android:name="com.instafree.InstaFreeSettings"
+            android:label="InstaFree"
+            android:exported="true"
+            android:theme="@android:style/Theme.DeviceDefault" />'''
+
+    if '</application>' not in content:
+        print("  Error: </application> not found in manifest")
+        return False
+
+    content = content.replace(
+        '</application>',
+        activity_tag + '\n    </application>'
+    )
+
+    with open(manifest_path, 'w') as f:
+        f.write(content)
+    print("  Registered InstaFreeSettings in AndroidManifest.xml")
+    return True
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: patch_manifest.py <AndroidManifest.xml>")
+        sys.exit(1)
+    if not patch_manifest(sys.argv[1]):
+        sys.exit(1)
